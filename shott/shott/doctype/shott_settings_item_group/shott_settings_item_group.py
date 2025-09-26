0c03b990fc3a5e4f75e7f23aabfdeb7e6e7ec5e1
jigar-greycube <jigar@greycube.in>
2025-09-26 09:52:55 +0530

    feat:shott setting item group doctype add

---
+++ shott/shott/doctype/shott_settings_item_group/shott_settings_item_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Greycube Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ShottSettingsItemGroup(Document):
+	pass
